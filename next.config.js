a41397d9abd60df1372b0931aa4bf2d812548f86
jjybaliguat <justinejeraldbaliguat@gmail.com>
2023-07-14 08:48:15 +0800

    some chamnges

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
     env: {
         nodeEnv: 'production',
         DEV_APP_API: 'http://localhost:5000',
-        PRODUCTION_APP_API: 'https://chat.rdnaksnds.com/',
+        PRODUCTION_APP_API: 'https://chat.rdnaksnds.com',
         DEV_APP_URL: 'http://localhost:3000',
         // APP_URL: 'https://rdnaksnds.com',
     }
